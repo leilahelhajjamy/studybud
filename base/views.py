3c3d1c4770a025e8cc73a14ae1d30a5a47b40eba
laila <lailahajjamy@gmail.com>
2022-02-17 23:24:52 +0100

    include navbar

--- base/views.py
+++ base/views.py
@@ -1,9 +1,8 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
 
 def home(request):
-    return HttpResponse('Home page')
+    return render(request, 'home.html')
 def room(request):
-    return HttpResponse('ROOM')
+    return  render(request,"room.html")
